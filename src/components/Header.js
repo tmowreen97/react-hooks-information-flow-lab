3f35d3278e05854dfd7b37c75fc7a5c477896f8a
Tasreen Mowreen <tmowreen97@gmail.com>
2022-09-09 12:06:13 -0400

    Done.

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header ({ onDarkModeClick, isDarkMode }){
+  return (
+    <header>
+      <h2>Shopster</h2>
+      <button onClick={onDarkModeClick} >{isDarkMode ? "Light" : "Dark"} Mode</button>
+    </header>
+  )
+}
+
+export default Header;
